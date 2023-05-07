f74e6aa6d28caeb66711547546380267d7a3c14a
Morten010 <97233551+Morten010@users.noreply.github.com>
2023-05-08 00:07:46 +0200

    date and weather added

--- src/components/InfoBoard.jsx
+++ src/components/InfoBoard.jsx
@@ -1,33 +1,36 @@
 
 import React, { useEffect, useState } from 'react'
+import useFetch from '../hooks/useFetch'
 
 export default function InfoBoard() {
-    const [data, setData] = useState(null)
-    const [weather, setWeather ] = useState(null)
+    const [date, setDate] = useState(new Date())
+    const { loading, data, error} = useFetch("https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fwww.dr.dk%2Fnyheder%2Fservice%2Ffeeds%2Fallenyheder%23")
+    const { loading: LoadingWeather, data: dataWeather, error: weatherError} = useFetch("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current_weather=true&hourly=temperature_2m,relativehumidity_2m,windspeed_10m")
 
-
-
-
-console.log("hey");    
-
+    useEffect(() => {
+        setInterval(() => setDate(new Date()), 10000);
+      }, []);
     
-
   return (
     <section id='infoBoard'>
         <div className="top">
             <h1><img src="/logo.svg" alt="TechCollege logo" /></h1>
             <div className="left">
                 <div className="time">
-                    22:07
+                    {date.toLocaleString("da-DK", {
+                         hour: 'numeric',
+                         minute: 'numeric',
+                         hour12: false,
+                    })}
                 </div> 
                 <div className="weather">
-                    7&deg;
+                    {dataWeather && dataWeather.current_weather.temperature}&deg;
                 </div>
             </div>
         </div>
 
         <div className="content">
-            {data && data.map(article => (
+            {data && data.items.slice(0, 2).map(article => (
                 <article key={article.guid}>
                     <h3>{article.title}</h3>
                     <address>{article.author} - {article.pubDate}</address>
---
+++ src/hooks/useFetch.js
@@ -0,0 +1,38 @@
+
+import React, { useEffect, useState } from 'react'
+
+
+const  useFetch = (url) => {
+    const [loading, setLoading] = useState(null)
+    const [data, setData] = useState(null)
+    const [error, setError] = useState(null)
+
+    useEffect(() => {
+
+       const fetchData = async () => {
+        setLoading(true)
+
+        try{
+            const res =  await fetch(url)
+            if(!res.ok){
+                throw new Error(res.statusText)
+            }
+            const json = await res.json()
+    
+            setLoading(false)
+            setData(json)
+            setError(null)
+        } catch(err) {
+            setLoading(false)
+            setError("Could not fetch the data")
+            console.log(err.message);
+        }
+       }
+
+       fetchData()
+    }, [url])
+
+    return { loading, data, error}
+}
+
+export default useFetch
